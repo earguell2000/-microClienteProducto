e3599589d1beca7bf5f49786fb163cad110fb5c7
earguell2000 <eduardo.arguellesbueno.st@everis.nttdata.com>
2022-04-05 12:52:03 +0200

    Cambios en echo service

--- EchoService/src/main/java/com/formacion/echo/client/controller/EchoServiceController.java
+++ EchoService/src/main/java/com/formacion/echo/client/controller/EchoServiceController.java
@@ -6,15 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/api/echo")
 public class EchoServiceController {
 	
-	@RequestMapping(value="/")
-	public String echoService() {
-		return "Echo Service";
-	}
-	
 	@GetMapping("/{id}")
-	public String echoService(@PathVariable String id) {
-		return id;		
+	public String echoService(@PathVariable(required=false) String id) {
+		String response = "";
+		if (id == null) {
+			response = "Echo Service";
+		} else {
+			response = id;
+		}
+		return response;		
 	}
 }
