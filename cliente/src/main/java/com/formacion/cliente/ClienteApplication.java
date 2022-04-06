1576e64fa4c41246f003a87698df0069be4ecedc
earguell2000 <eduardo.arguellesbueno.st@everis.nttdata.com>
2022-04-06 10:29:49 +0200

    Cambios para desplegar el micro de clientes

--- cliente/src/main/java/com/formacion/cliente/ClienteApplication.java
+++ cliente/src/main/java/com/formacion/cliente/ClienteApplication.java
@@ -2,14 +2,10 @@ package com.formacion.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClienteApplication {
 
 	public static void main(String[] args) {
--- cliente/src/test/java/com/formacion/cliente/ClienteApplicationTests.java
+++ cliente/src/test/java/com/formacion/cliente/ClienteApplicationTests.java
@@ -1,13 +1,7 @@
 package com.formacion.cliente;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClienteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
